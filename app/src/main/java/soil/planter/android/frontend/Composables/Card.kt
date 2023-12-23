435df7026646459334583b339430167b0400937d
CodeC <58306894+CodeCLS@users.noreply.github.com>
2023-12-23 01:24:56 +0100

    small changes to encyclopediapage

--- app/src/main/java/soil/planter/android/frontend/Composables/Card.kt
+++ app/src/main/java/soil/planter/android/frontend/Composables/Card.kt
@@ -15,6 +15,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
@@ -178,7 +179,7 @@ fun EncyclopediaCardItem(
     name: String,
     description: String
 ) {
-    ImageCard(painter = painter, contentDescription = description, modifier = Modifier.size(125.dp))
+    ImageCard(painter = painter, contentDescription = description, modifier = Modifier.wrapContentSize())
     Text(name, fontSize = 16.sp,
         fontFamily = FontFamily(Font(R.font.k2d_medium)),
         fontWeight = FontWeight(400),
